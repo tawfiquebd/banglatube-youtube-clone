1fd3e4bab07a318411cee1470765af8fdefb7267
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-08-14 15:51:53 +0600

    VideoProcessor.php class added

--- processing.php
+++ processing.php
@@ -1,9 +1,18 @@
 <?php
+require_once ("includes/header.php");
+require_once ("includes/classes/VideoUploadData.php");
+require_once ("includes/classes/VideoProcessor.php");
+
 if(!isset($_POST['uploadButton']))    {
     echo "No file sent to page";
     exit();
 }
 
+// create file upload data
 $videoUploadData = new VideoUploadData($_POST["fileInput"], $_POST["titleInput"], $_POST["descriptionInput"], $_POST["privacyInput"], $_POST["categoryInput"], "REPLACE-THIS");
+
+// process video data (upload)
+$videoProcessor = new VideoProcessor($con);
+$wasSuccessful = $videoProcessor->upload($videoUploadData);
 
 ?>
