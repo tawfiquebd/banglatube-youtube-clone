9e01d4f64ef7d926a122ead46ff2ec3d35c7f1fd
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-08-14 17:59:26 +0600

    Video size handling

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -2,6 +2,7 @@
 class VideoProcessor{
 
     private $con;
+    private $sizeLimit = 500000000;
 
     public function __construct($con){
         $this->con = $con;
@@ -13,7 +14,22 @@ class VideoProcessor{
 
         $tempFilePath = $targetDir . uniqid() . basename($videoData['name']);
         $tempFilePath = str_replace(" ","_",$tempFilePath);
+
+        $isValidData = $this->processData($videoData, $tempFilePath);
         echo $tempFilePath;
+    }
+
+    public function processData($videoData, $filePath){
+        $videoType = pathinfo($filePath, PATHINFO_EXTENSION); // built in function
+
+        if(!$this->isValidSize($videoData)){
+            echo "File too large. Can't be more than ".$this->sizeLimit ." bytes";
+            return false;
+        }
+    }
+
+    public function isValidSize($data){
+        return $data["size"] <= $this->sizeLimit;
     }
 }
 ?>
