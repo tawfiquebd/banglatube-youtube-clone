501674064096ab9bf6085105e074be7aa640c463
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-08-25 01:45:16 +0600

    Outputting the video player & created VideoPlayer class

--- watch.php
+++ watch.php
@@ -1,16 +1,24 @@
 <?php
 require_once("includes/header.php");
-require_once("includes/classes/Video.php");
+require_once("includes/classes/VideoPlayer.php");
 
 if(!isset($_GET["id"])) {
     echo "No url passed into page";
     exit();
 }
 
-$video = new Video($con, $_GET['id'], $userLoggedInObj);
+$video = new Video($con, $_GET['id'], $userLoggedInObj);    // video object creating
 
 ?>
 
+<div class="watchLeftColumn">
+
+<?php
+    $videoPlayer = new VideoPlayer($video); // passing video object
+    echo $videoPlayer->create(true);
+?>
+
+</div>
 
 
 <?php require_once("includes/footer.php"); ?>
