f82a22479fea2fcf820135978040d17d7acae3f3
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-08-29 22:14:50 +0600

    Edit video button

--- includes/classes/ButtonProvider.php
+++ includes/classes/ButtonProvider.php
@@ -13,6 +13,18 @@ class ButtonProvider {
                 </button>";
     }
 
+    public static function createHyperlinkButton($text, $imageSrc, $href, $class) {
+
+        $image = ($imageSrc == null) ? "" : "<img src='$imageSrc' alt='button image'>";
+
+        return "<a href='$href'>
+                    <button class='$class'>
+                        $image
+                        <span class='text'>$text</span>
+                    </button>
+                </a>";
+    }
+
     public static function createUserProfileButton($con, $username) {
         $userObj = new User($con, $username);
         $profilePic = $userObj->getProfilePic();
@@ -21,6 +33,16 @@ class ButtonProvider {
         return "<a href='$link' > 
                     <img src='$profilePic' class='profilePicture' alt='Profile Picture'>
                 </a>";
+    }
+
+    public static function createEditVideoButton($videoId){
+        $href = "editVideo.php?videoId=$videoId";
+
+        $button = ButtonProvider::createHyperlinkButton("EDIT VIDEO", null, $href, "edit button");
+
+        return "<div class='editVideoButtonContainer'>
+                    $button
+                </div>";
     }
 
 
