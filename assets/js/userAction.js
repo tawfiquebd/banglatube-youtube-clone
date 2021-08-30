2d248812dcfdc7761a565a088075aba416966a74
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-08-30 16:26:43 +0600

    Updating the text of subscribe button

--- assets/js/userAction.js
+++ assets/js/userAction.js
@@ -5,7 +5,16 @@ function subscribe(userTo, userFrom, button) {
     }
 
     $.post('ajax/subscribe.php', {userTo : userTo, userFrom : userFrom})
-        .done(function(data){
-            console.log(data);
+        .done(function(count){
+
+            if(count != null){
+                $(button).toggleClass("subscribe unsubscribe");
+
+                var buttonText = $(button).hasClass("subscribe") ? "SUBSCRIBE" : "SUBSCRIBED";
+                $(button).text(buttonText + " " + count);
+            }
+            else{
+                alert("something went wrong");
+            }
         });
 }
