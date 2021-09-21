3e62385c4bf7c6d7dd1ad5ad75e56c61b998d3c5
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-09-22 00:35:52 +0600

    Inserting comments into database

--- ajax/postComment.php
+++ ajax/postComment.php
@@ -2,7 +2,19 @@
 require_once("../includes/config.php");
 
 if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['videoId'])) {
-    echo "Successfull";
+    $query = $con->prepare("INSERT INTO comments(posted_by, video_id, response_to, body) 
+                                VALUES(:postedBy, :videoId, :responseTo, :body)");
+    $query->bindParam(":postedBy", $postedBy);
+    $query->bindParam(":videoId", $videoId);
+    $query->bindParam(":responseTo", $responseTo);
+    $query->bindParam(":body", $body);
+
+    $postedBy   = $_POST['postedBy'];
+    $videoId    = $_POST['videoId'];
+    $responseTo = $_POST['responseTo'];
+    $body       = $_POST['commentText'];
+
+    $query->execute();
 }
 else{
     echo "One or more parameters are not passed into subscribe.php file";
