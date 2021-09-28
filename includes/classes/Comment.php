523384a34e34c5eea31382596b62128976254702
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-09-28 23:27:54 +0600

    Created comment timestamp

--- includes/classes/Comment.php
+++ includes/classes/Comment.php
@@ -25,7 +25,7 @@ class Comment {
         $body = $this->sqlData['body'];
         $postedBy = $this->sqlData['posted_by'];
         $profileButton = ButtonProvider::createUserProfileButton($this->con, $postedBy);
-//        $timespan = "";
+        $timespan = $this->time_elapsed_string($this->sqlData["date_posted"]);
 
         $commentControls = new CommentControls($this->con, $this, $this->userLoggedInObj);
         $commentControls = $commentControls->create();
@@ -39,7 +39,7 @@ class Comment {
                                 <a href='profile.php?username=$postedBy'>
                                     <span class='username'>$postedBy</span>
                                 </a>
-                                <span class='timestamp'></span>
+                                <span class='timestamp'>$timespan</span>
                             </div>
                             
                             <div class='body'>
@@ -49,6 +49,35 @@ class Comment {
                     </div>
                     $commentControls
                 </div>";
+    }
+
+    function time_elapsed_string($datetime, $full = false) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
     }
 
     public function getId() {
