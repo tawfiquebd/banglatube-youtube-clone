af4606e49f9f587125d6c7c9f66e7ce61c35371f
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-10-09 12:07:44 +0600

    VideoGrid class created

---
+++ includes/classes/VideoGrid.php
@@ -0,0 +1,18 @@
+<?php
+class VideoGrid {
+
+    private $con, $userLoggedInObj;
+    private $largeMode = false;
+    private $gridClass = "videoGrid";
+
+    public function __construct($con, $userLoggedInObj) {
+        $this->con = $con;
+        $this->userLoggedInObj = $userLoggedInObj;
+    }
+
+    public function create($videos, $title, $showFilter) {
+        return "<div class='$this->gridClass'>
+
+                </div>";
+    }
+}
