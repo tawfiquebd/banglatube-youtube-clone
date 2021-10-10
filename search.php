eae66a78d96f74184e6688df49897f9f2ad801ad
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-10-10 20:19:19 +0600

    Search page parameters

---
+++ search.php
@@ -0,0 +1,26 @@
+<?php
+require_once ('includes/header.php');
+?>
+
+<?php
+
+if(!isset($_GET['term']) || $_GET['term'] == '') {
+    echo "You must enter a search term";
+    exit();
+}
+
+$term = $_GET["term"];
+
+if(!isset($_GET["orderBy"]) || $_GET["orderBy"] == "views") {
+    $orderBy = "views";
+}
+else {
+    $orderBy = "uploadDate";
+}
+
+?>
+
+
+<?php
+require_once ('includes/footer.php');
+?>
